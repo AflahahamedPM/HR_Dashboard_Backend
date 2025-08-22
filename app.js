85a3aedb1a059093394ef465b55d7ab07d5dcd2a
AflahahamedPM <aflah777@gmail.com>
2025-08-22 18:39:07 +0530

    made changes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:5173",
+      "https://hr-dashboard-frontend-4ewj.vercel.app"
     ],
     credentials: true,
   })
